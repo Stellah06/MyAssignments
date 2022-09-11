9404fa67886ef20dbd08f45e47a4f1d584b8d220
Stellah06 <110719074+Stellah06@users.noreply.github.com>
2022-09-11 08:59:15 +0530

    Add files via upload
    LearnAnnotations

---
+++ testcases/LearnAnnotation.java
@@ -0,0 +1,30 @@
+package testcases;
+
+import org.testng.annotations.*;
+
+public class LearnAnnotation {
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("Before Class");
+    }
+    @AfterClass
+    public void afterClass(){
+        System.out.println("After Class");
+    }
+    @AfterSuite
+    public void afterSuite(){
+        System.out.println("After Suite");
+    }
+    @BeforeSuite
+    public void beforeSuite(){
+        System.out.println("Before Suite");
+    }
+    @BeforeTest
+    public void beforeTest(){
+        System.out.println("Before Test");
+    }
+    @AfterTest
+    public void afterTest(){
+        System.out.println("After Test");
+    }
+}
